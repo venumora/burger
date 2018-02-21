261d02c296ae1365236c2f1e1e8fb777c2eee9ef
venu.mora <venukumar.mora@gmail.com>
2018-02-21 01:02:16 -0800

    remove unwanted code

--- public/js/index.js
+++ public/js/index.js
@@ -13,15 +13,3 @@ $(document).ready(function() {
     });
   });
 });
-
-module.exports.register = function(Handlebars, options) {
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
-};

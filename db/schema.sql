c71741740ac0e5b2296b4abac9d335a0ff6818fa
venu.mora <venukumar.mora@gmail.com>
2018-02-21 00:56:46 -0800

    Update table schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ USE burgers_db;
 
 CREATE TABLE burgers 
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(60) NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(60) NOT NULL,
     devoured TINYINT NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
